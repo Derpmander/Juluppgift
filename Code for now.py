aa38443a1ef1cd658d9aece826c9501ee4adc0ee
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-12-07 14:23:29 +0100

    Update Code for now.py

--- Code for now.py
+++ Code for now.py
@@ -1,5 +1,5 @@
 image Rika icon1 = "Rika_Transparent.png"
-image Santa icon1 = "Jultomten.jpg"
+image Santa icon1 = "Jultomten_Transparent.jpg"
 image Tree = "Tree.png"
 
 define r = Character("Rika", color="#c8ffc8")
