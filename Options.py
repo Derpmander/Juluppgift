eaf6c5119c2761f9cd4aabd1f6c9a56887888e3b
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-12-14 12:22:08 +0100

    Lade till fixad kod

--- Options.py
+++ Options.py
@@ -1,5 +1,16 @@
-define config.version = "1.2a" #Definierar vilken version spelet är på huvudmenyn
+define config.version = "1.4a" #Definierar vilken version spelet är på huvudmenyn
 
+define gui.about = _p(""" Character Art: Erik.
+
+Original Character Art: Erik
+
+Background Art: Erik
+
+Original Background Art: Erik
+
+Music By: Erik
+
+Written By: Erik""")
 
 
 define config.window_icon = "gui/Jorden.png"#Ändrar Ikonen på spelet i taskbar och fönstret
--- screens.py
+++ screens.py
@@ -11,3 +11,24 @@ screen patch_notes():
 
         has vbox:
             spacing 20
+        text _("{b}Patch 1.4a:{/b} Bug fixes")
+
+        text _("{b}Patch 1.3b:{/b} Added the final ending")
+
+        text _("{b}Patch 1.3a:{/b} Added a good ending")
+
+        text _("{b}Patch 1.2c:{/b} Added the cave and a inabitant of such cave")
+
+        text _("{b}Patch 1.2b:{/b} Added the tent and a new bad ending along one of the paths")
+
+        text _("{b}Patch 1.2a:{/b} Added the road to the circus, the random hobo, the clown and the hobos alterego ")
+
+        text _("{b}Patch 1.1e:{/b} Added more dialogue a new bad ending, a new 'fancy' background and more info in the about page.")
+
+        text _("{b}Patch 1.1d:{/b} Added patch notes screen, changed the window icon and a mysterious figure.")
+
+        text _("{b}Patch 1.1c:{/b} Added a bad ending, rika's mother and changed the name of preferences too settings.")
+
+        text _("{b}Patch 1.1b:{/b} Changed the main menu and pause screen background and added nice textboxes, the narrator and dialogue.")
+
+        text _("{b}Patch 1.1a:{/b} Added Rika, Santa, two backgrounds and test dialogue ")
